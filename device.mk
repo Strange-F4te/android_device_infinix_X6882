3047a99dd2adf3384a98cafa00e4c2cd57262956
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-11 09:06:56 +0700

    X6882: Add keylayout file to map double tap event to wake
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: Miracleprjkt <Miraclev1@proton.me>

--- device.mk
+++ device.mk
@@ -11,5 +11,9 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/mtk-tpd.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-tpd.kl
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X6882/X6882-vendor.mk)
