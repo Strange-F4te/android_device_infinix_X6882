44f073f05f1cdc99e7299090b67a9a8ee42ebe5f
Miracleprjkt <Miraclev1@proton.me>
2025-08-22 13:03:12 +0700

    X6882: Import rootdir from X6882-H8915ABCDFGHIJLMNOPRVWX-U-OP-250616V1395
    Signed-off-by: Miracleprjkt <Miraclev1@proton.me>

--- device.mk
+++ device.mk
@@ -18,5 +18,35 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/mtk-tpd.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-tpd.kl
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.insmod.sh \
+    init.spd_cpu_set.sh \
+    init.tasksetEnd.sh \
+    init.tasksetStart.sh \
+    init.tran_mcf.sh \
+    factory_init.connectivity.common.rc \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6789.rc \
+    init.mt6789.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    init_connectivity.rc \
+    meta_init.connectivity.common.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    meta_init.vendor.rc \
+    multi_init.rc 
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X6882/X6882-vendor.mk)
